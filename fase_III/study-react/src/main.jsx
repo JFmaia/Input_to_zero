4b7c340c4dc32964fa428612af811659448497e6
JFmaia <jfmaia741@gmail.com>
2023-04-16 21:06:28 -0300

    feat: Modified root of touch.

--- fase_III/study-react/src/main.jsx
+++ fase_III/study-react/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Home } from './pages/Home'
+import { Home } from './pages/Home/home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <Home/>
   </React.StrictMode>,
 )
