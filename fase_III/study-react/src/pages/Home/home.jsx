294e4f5020221df54cf324bfcec09f31673f906a
JFmaia <jfmaia741@gmail.com>
2023-04-16 21:08:36 -0300

    feat: replacement name

---
+++ fase_III/study-react/src/pages/Home/home.jsx
@@ -0,0 +1,17 @@
+// Pegando valores pelo props do componente
+function Title({children, className='teste'}){
+    return <h1 className={className}>{children}</h1>
+}
+// Componete react
+export function Home(){
+    const title = "Olá José!";
+    const className = "title";
+
+    return (
+        <div>
+            <Title>
+                Olá React!
+            </Title>
+        </div>
+    )
+}
