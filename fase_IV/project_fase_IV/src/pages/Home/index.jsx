6172b0849811f50ab88e884213cbfbd67bd3ece1
JFmaia <jfmaia741@gmail.com>
2023-08-07 09:17:37 -0300

    Criando um contador com userState!

--- fase_IV/project_fase_IV/src/pages/Home/index.jsx
+++ fase_IV/project_fase_IV/src/pages/Home/index.jsx
@@ -1,21 +1,55 @@
 // import { useState } from 'react'
 import { useState, useEffect } from 'react'
-import './home.css'
-export function Home() {
-  const [currentTime, updateCurrentTime] = useState(new Date().toLocaleTimeString())
-  useEffect(() => {
-    const interval = setInterval(()=>{
-      let novoValor = new Date().toLocaleTimeString()
-      updateCurrentTime(novoValor)
-    }, 1000) 
+// import './home.css'
+// export function Home() {
+//   const [currentTime, updateCurrentTime] = useState(new Date().toLocaleTimeString())
+//   useEffect(() => {
+//     const interval = setInterval(()=>{
+//       let novoValor = new Date().toLocaleTimeString()
+//       updateCurrentTime(novoValor)
+//     }, 1000) 
 
+//     return () => {
+//       clearInterval(interval)
+//     }
+//   }, [])
+//   return (
+//     <div className="container">
+//       <div id='clock'> {currentTime} </div> 
+//     </div>
+//   )
+// }
+
+function Counter() {
+  const [total, setTotal] = useState(0)
+
+  const add = () => setTotal(total + 1)
+  const remove = () => setTotal(total - 1)
+
+  useEffect(() => {
+    console.log("Contador Criado")
     return () => {
-      clearInterval(interval)
+      console.log("Contador removido")
     }
   }, [])
-  return (
-    <div className="container">
-      <div id='clock'> {currentTime} </div> 
+  return <div>
+    <button onClick={add}>Adicionar</button>
+    <button onClick={remove}>Remover</button>
+    <div>{total}</div>
+  </div>
+}
+
+
+
+export function Home() {
+  const [enabled, setEnabled] = useState(false)
+  const toggleCounter = () => {
+    setEnabled(!enabled)
+  }
+  return(
+    <div className='container'>
+      <button onClick={toggleCounter}>{enabled ? "Esconder o contador" : "Mostrar o contador"}</button>
+      {enabled ? <Counter /> : ''}
     </div>
   )
 }
