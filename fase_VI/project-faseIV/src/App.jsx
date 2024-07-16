b6d39ac7978b9e4c38aebfced613007a04735c40
JFmaia <jfmaia.dev@gmail.com>
2024-07-15 23:01:16 -0300

    Project react + tailwind

---
+++ fase_VI/project-faseIV/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-row min-w-full justify-center items-center min-h-full bg-[#100626] text-white font-bold text-4xl">
+      <p>Hellou Word</p>
+    </div>
+  )
+}
+
+export default App
